61ef88023484b06f8a9e028233e0c0e3769c7e5a
NoakPalander <noak.palander@gmail.com>
2022-12-04 23:44:40 +0100

    day 4 in c++

--- Day4/solution.cpp
+++ Day4/solution.cpp
@@ -2,35 +2,25 @@
 #include <filesystem>
 #include <fstream>
 #include <ranges>
-#include <string_view>
 #include <vector>
 #include <span>
+#include <cstdio>
 #include <iostream>
 
 namespace stdfs = std::filesystem;
 namespace stdr = std::ranges;
+using range_t = std::ranges::iota_view<int, int>;
 
-std::vector<int> range(std::string_view segment) {
-    auto sep = segment.begin() + segment.find('-');
-    std::string_view begin(segment.begin(), sep);
-    std::string_view end(sep + 1, segment.end());
-
-    int first = std::stoi(begin.data());
-    int second = std::stoi(end.data());
-
-    auto it = std::views::iota(first, second + 1);
-    return std::vector<int>(it.begin(), it.end());
-}
-
-int input(stdfs::path const& path, int(*part)(std::span<int>, std::span<int>, std::span<int>)) {
+int input(stdfs::path const& path, int(*part)(range_t, range_t, std::span<int>)) {
     int total = 0;
     std::ifstream reader(path);
 
     for (std::string line; std::getline(reader, line);) {
-        auto div = line.begin() + line.find(',');
+        int a, b, c, d;
+        std::sscanf(line.c_str(), "%d-%d,%d-%d", &a, &b, &c, &d);
 
-        std::vector<int> task1 = range({line.begin(), div});
-        std::vector<int> task2 = range({div + 1, line.end()});
+        auto task1 = std::views::iota(a, b + 1);
+        auto task2 = std::views::iota(c, d + 1);
 
         std::vector<int> intersection;
         stdr::set_intersection(task1, task2, std::back_inserter(intersection));
@@ -40,11 +30,11 @@ int input(stdfs::path const& path, int(*part)(std::span<int>, std::span<int>, st
     return total;
 }
 
-int part_one(std::span<int> task1, std::span<int> task2, std::span<int> intersection) {
+int part_one(range_t task1, range_t task2, std::span<int> intersection) {
     return stdr::equal(task1, intersection) || stdr::equal(task2, intersection);
 }
 
-int part_two([[maybe_unused]] std::span<int> task1, [[maybe_unused]]  std::span<int> task2, std::span<int> intersection) {
+int part_two([[maybe_unused]] range_t task1, [[maybe_unused]] range_t task2, std::span<int> intersection) {
     return !intersection.empty();
 }
 
