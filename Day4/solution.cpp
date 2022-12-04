1a3de259f26c608787c91bda64f70d920078e05e
NoakPalander <noak.palander@gmail.com>
2022-12-04 23:33:48 +0100

    day 4 in c++

--- Day4/solution.cpp
+++ Day4/solution.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 
 namespace stdfs = std::filesystem;
+namespace stdr = std::ranges;
 
 std::vector<int> range(std::string_view segment) {
     auto sep = segment.begin() + segment.find('-');
@@ -20,7 +21,7 @@ std::vector<int> range(std::string_view segment) {
     return std::vector<int>(it.begin(), it.end());
 }
 
-int part_one(stdfs::path const& path) {
+int input(stdfs::path const& path, int(*part)(std::span<int>, std::span<int>, std::span<int>)) {
     int total = 0;
     std::ifstream reader(path);
 
@@ -31,35 +32,23 @@ int part_one(stdfs::path const& path) {
         std::vector<int> task2 = range({div + 1, line.end()});
 
         std::vector<int> intersection;
-        std::ranges::set_intersection(task1, task2, std::back_inserter(intersection));
-        if (intersection == task1 || intersection == task2)
-            ++total;
+        stdr::set_intersection(task1, task2, std::back_inserter(intersection));
+        total += part(task1, task2, intersection);
     }
 
     return total;
 }
 
-int part_two(stdfs::path const& path) {
-    int total = 0;
-    std::ifstream reader(path);
+int part_one(std::span<int> task1, std::span<int> task2, std::span<int> intersection) {
+    return stdr::equal(task1, intersection) || stdr::equal(task2, intersection);
+}
 
-    for (std::string line; std::getline(reader, line);) {
-        auto div = line.begin() + line.find(',');
-
-        std::vector<int> task1 = range({line.begin(), div});
-        std::vector<int> task2 = range({div + 1, line.end()});
-
-        std::vector<int> intersection;
-        std::ranges::set_intersection(task1, task2, std::back_inserter(intersection));
-        if (!intersection.empty())
-            ++total;
-    }
-
-    return total;
+int part_two([[maybe_unused]] std::span<int> task1, [[maybe_unused]]  std::span<int> task2, std::span<int> intersection) {
+    return !intersection.empty();
 }
 
 int main() {
     auto path = stdfs::current_path().parent_path() / "input.txt";
-    std::cout << "Part one: " << part_one(path) << '\n';
-    std::cout << "Part two: " << part_two(path) << '\n';
+    std::cout << "Part one: " << input(path, part_one) << '\n';
+    std::cout << "Part two: " << input(path, part_two) << '\n';
 }
